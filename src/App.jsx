709db84753c04992a124fa9efbc7b4e3d862f36f
killlercam <herosviral@gmail.com>
2025-03-30 15:18:44 +0530

    Created a navbar and made it responsive too

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-
-import { useState } from 'react';
-import  styles from './App.module.css';
-import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from "react";
+import styles from "./App.module.css";
+import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
-
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   //For showing if we are still loading or not
-const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <div className={`${styles.App} min-h-screen`}>
-    {!isLoaded && <LoadingScreen onComplete = {() => setIsLoaded(true)}/>}{" "}
-      <div className = {` transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black` } ></div>
-     
+    /* <div className={`${styles.App} min-h-screen`}>*/
+    <div className={`${styles.App}`}>
+      {/* Showing the loading Screen untill the screen is loaded */}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {/* Fadding Transition effect after the loading is finished */}
+      <div
+        className={` transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } `}
+      >
+        
+      <Navbar/>
+
+      </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  /*creating state for managing the state for making responsive*/
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a href="/" className={styles.title}>
+        Porfolio
+      </a>
+      <div className={styles.menu}>
+        {/* Adding Image */}
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen 
+            ? "/assets/nav/closeIcon.png" 
+            : "/assets/nav/menuIcon.png" 
+          }
+          alt="Menu Button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+
+        <ul className={`${menuOpen && styles.menuOpen} ${styles.menuItems} `}
+        onClick={() => setMenuOpen(false)}>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/experience">Experience</a>
+          </li>
+          <li>
+            <a href="/project">Project</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
